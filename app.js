da0e4219e98f3d13895c3525b80f5f54880e92b1
ikwebacount <155137248+ikwebacount@users.noreply.github.com>
2024-03-09 16:38:09 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal('.main2title',{
+    distance:'50px',
+    delay:200,
+    duration:800,
+    opacity:0.6,
+    easing:'ease-in-out',
+    interval:400,
+});
+
+ScrollReveal().reveal('.holleMessage p, .useLung li, .lungLevel li, .toWont li',{
+    distance:'30px',
+    delay:300,
+    duration:1000,
+    opacity:0.0,
+    easing:'ease-out',
+    interval:400,    
+    origin:'left',
+});
+
+
+ScrollReveal().reveal('.kao',{
+    distance:'120px',
+    delay:300,
+    duration:800,
+    opacity:0.1,
+    easing:'ease-out',
+    interval:400,    
+    origin:'top',
+});
